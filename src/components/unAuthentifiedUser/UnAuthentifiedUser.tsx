ec3d890a6557cf2471d2c42fab3af88423dcc37f
Michel-Developpement <a.copiercoller@gmail.com>
2024-05-27 20:36:33 +0200

    merge in main

--- src/components/unAuthentifiedUser/UnAuthentifiedUser.tsx
+++ src/components/unAuthentifiedUser/UnAuthentifiedUser.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-// import { CardDisplay } from "./Card";
 import Home from "./home/Home";
 import LoginUser from "./login/LoginUser";
 import SignUpUser from "./signUp/SignUpUser";
@@ -10,7 +9,6 @@ const UnAuthentifiedUser = () => {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<LoginUser />} />
       <Route path="/sign-up" element={<SignUpUser />} />
-      {/* <Route path="/:emailUser" element={<CardDisplay />} /> */}
     </Routes>
   );
 };
