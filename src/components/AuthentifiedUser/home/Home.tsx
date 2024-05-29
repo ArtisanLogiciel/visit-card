28ade90676161e4d347236b8845073420aab783b
Nabil Benguernane <nabil.benguernane@gmail.com>
2024-05-29 15:17:39 +0200

    ajout d'un error boundary. Un composant ErrorThrower  se trouve dans main.tsx pour leve une erreur lors du montage

--- src/components/AuthentifiedUser/home/Home.tsx
+++ src/components/AuthentifiedUser/home/Home.tsx
@@ -26,6 +26,7 @@ const Home = () => {
       <EditCardButton/>
 
       <ViewCardButton/>
+      <button onClick={()=>{throw new Error ("erreur fictif")}}>Lever une erreur</button>
       
 
       <br />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App.tsx";
+import FallbackComponent from "./components/FallbackComponent.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ErrorBoundary FallbackComponent={FallbackComponent}>
+        <App />
+        {/* Le composant ErrorThrower permet de déclencher une erreur pour tester le error boundary */}
+        {/* <ErrorThrower/> */}
+      </ErrorBoundary>
     </Router>
   </React.StrictMode>
 );
