36e49e46716d9d748b744a525588f3ee8a822e6e
Michel-Developpement <a.copiercoller@gmail.com>
2024-06-06 11:58:09 +0200

    qrcode implemementation

---
+++ src/components/elements/qrcode/DisplayQrCode.tsx
@@ -0,0 +1,29 @@
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import QRCode from "qrcode.react";
+import { useEffect, useState } from "react";
+
+export const DisplayQrCode = () => {
+  const [user, setUser] = useState<User | null | string>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser?.email);
+      }
+      setUser(null);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  return (
+    <div className="flex flex-col justify-center items-center w-sreen h-screen overflow-hidden">
+      <h1 className="text-2xl mb-7"> votre Qrcode à partager</h1>
+      <QRCode
+        value={`http://visit-card.online/display-card/${user}`}
+        renderAs="canvas"
+      />
+    </div>
+  );
+};
